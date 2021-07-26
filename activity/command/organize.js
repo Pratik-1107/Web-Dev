89b9407877b09841796ce4617c97c6c97f2b2e53
Pratik-1107 <pratikhkarad11@gmail.com>
2021-07-26 14:09:24 +0530

    Completed FIle Organizer and Tree

---
+++ activity/command/organize.js
@@ -0,0 +1,57 @@
+
+let fs = require("fs");
+let path = require("path");
+
+let types = {
+    Media : [".mp4", '.mkv',".png",".jpeg",".jpg",""],
+    Archives : [".zip",".7z",".rar",".tar",".gz",".ar",".iso"],
+    Documents : [".doc",".docx",".pdf",".xlsx",".xls",".odt",".pptx",".ppt"],
+    App : [".exe",".dmg",".pkg",".deb"]
+};
+
+function fn(rPath)
+{
+
+    let currPath = rPath;
+    let content = fs.readdirSync(currPath);
+    let organizedFolderPath = path.join(currPath, "Organised_Folder");
+    fs.mkdirSync(organizedFolderPath);
+
+    for(let i = 0;i < content.length;i++)
+    {
+        let currFile = content[i];
+        let extName = path.extname(currFile);
+        let eleType = "Other";
+        for(const key in types)
+        {
+            for(let j = 0;j < types[key].length;j++)
+            {
+                if(extName == types[key][j])
+                {
+                    eleType = key;
+                    break;
+                }
+            }
+            if(eleType != "Other")
+            {
+                break;
+            }
+        }
+        let folderPath = path.join(organizedFolderPath,eleType);
+        let srcFilePath = path.join(currPath,currFile);
+
+        let isFileExistes = fs.existsSync(folderPath);
+        if(!isFileExistes)
+        {
+            fs.mkdirSync(folderPath);
+        }
+        let destFilePath = path.join(folderPath,currFile);
+
+        fs.copyFileSync(srcFilePath,destFilePath);
+        console.log(currFile, "belongs to --> ", eleType);
+    }
+}
+
+module.exports = {
+    fxn : fn
+}
